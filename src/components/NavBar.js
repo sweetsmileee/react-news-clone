ffac3f6011c855bff00b67892ec7401dd5284174
MattToczek <52243130+MattToczek@users.noreply.github.com>
2019-11-27 12:21:27 +0000

    Create NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export default function NavBar(props) {
+    return (
+    
+        <nav className="navBar">
+            <div className="burgerMenu">
+            <img src={props.burger} />
+                </div>
+                <input type="text" placeholder="Search the code nation DIRT"></input>
+                <div className="searchIcon">
+            <img className="searchImg" src={props.search} />
+            </div>
+        </nav> 
+    );
+}
