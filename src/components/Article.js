6789509e13f4bffb1b91939e6c128fe58d3b9231
sweetsmileee <55324343+sweetsmileee@users.noreply.github.com>
2019-11-29 14:41:58 +0000

    Revert "Eric"

--- src/components/Article.js
+++ src/components/Article.js
@@ -7,7 +7,7 @@ export default function Article(props) {
             <div className="textArticle">
                 <h2 className="articleH2">A newfound black hole</h2>
                 <p className="articleP">The black hole is locked in orbit with a young blue star dubbed LB-1, which sits about 13,800 light-years away in the constellation Gemini, researchers found.</p>
-                {/* <a className="mybtnArticle">Read More</a> */}
+                <a className="mybtnArticle">Read More</a>
             </div>
             <div className="wrapperArticle">
                 <img className="blackholeImage" src={blackhole} />
